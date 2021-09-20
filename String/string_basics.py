0325545238abb0390462977e9f652ef43d37ee88
gaurav <gauravkamble74@gmail.com>
2021-09-20 18:06:25 +0530

    basic string programs

---
+++ String/string_basics.py
@@ -0,0 +1,8 @@
+# making a string
+name1="tanu"
+name2='dia'
+name3= '''tanay'''
+print(name3)
+
+
+
---
+++ String/string_methods.py
@@ -0,0 +1,30 @@
+movie_name="kgf Chapter 2"
+
+# print(movie_name)
+
+# length of string
+# print(len(movie_name))
+
+# title 
+# print(movie_name.title())
+
+# upper
+# print(movie_name.upper())
+
+# lower
+# print(movie_name.lower())
+
+# ends with
+# print(movie_name.endswith(" "))
+
+# counts
+# print(movie_name.count("a"))
+
+# capitalize
+# print(movie_name.capitalize())
+
+# find
+# print(movie_name.find("Chapter"))
+
+# replace
+# print(movie_name.replace("2","3"))
