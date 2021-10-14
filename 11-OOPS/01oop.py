2e410d4b4911d2f8a41eac4a6256f11309513a61
gauravkamble74 <gauravkamble74@gmail.com>
2021-10-14 18:06:50 +0530

    oops

---
+++ 11-OOPS/01oop.py
@@ -0,0 +1,27 @@
+class Number:
+    def sum(self):
+        return self.a+self.b
+
+num = Number()
+num.a = 5
+num.b = 4
+
+s = num.sum()
+print(s)
+
+num1 = Number()
+num1.a = 12
+num1.b = 11
+
+s1 = num1.sum()
+print(s1)
+
+# def sum(a,b):
+#     return a+b
+# c = sum(5,4)
+# print(c)    
+
+# a = 5
+# b = 4
+
+# print(a+b)
---
+++ test.py
@@ -0,0 +1,15 @@
+# class Number:
+#     def sum(self):
+#         return self.a+self.b 
+
+# num = Number()
+# num.a = 12
+# num.b = 34
+# s=num.sum()
+# print(s)
+
+a = 5
+
+a = "gaurav"
+
+print(a)
